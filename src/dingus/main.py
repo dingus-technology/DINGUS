51882a1bf71c4e8addb4ab317ab9ed60c54ae06b
Leon Watson <leonwatson9@icloud.com>
2025-02-19 08:44:52 +0000

    🖼️ Feat: enhance FastAPI app with custom title and Swagger UI configuration

--- src/dingus/main.py
+++ src/dingus/main.py
@@ -4,14 +4,22 @@ Main FastAPI entrypoint
 """
 
 from fastapi import FastAPI, status
+from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 
 from dingus.routers.chat import router as chat_router
 
-app = FastAPI()
+APP_TITLE = "DINGUS | Chat with Logs"
 
-
+app = FastAPI(docs_url=None, redoc_url=None, title=APP_TITLE)
 app.include_router(chat_router)
+app.mount("/assets", StaticFiles(directory="/assets"), name="assets")
+
+
+@app.get("/docs", include_in_schema=False)
+def overridden_swagger():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title=APP_TITLE, swagger_favicon_url="/assets/favicon.ico")
 
 
 @app.get("/health")
