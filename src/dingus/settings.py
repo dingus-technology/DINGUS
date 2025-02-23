196631ce70780c4ffdfa39ce20a45e5b81407c4c
Leonwatsoni <leonhunterwatson@gmail.com>
2025-02-23 09:33:19 +0000

    ⚙️ feat: Update Docker configuration and environment variables for Qdrant integration; enhance settings for model and logging

--- src/dingus/settings.py
+++ src/dingus/settings.py
@@ -5,6 +5,8 @@ Settings for the Dingus app.
 
 import os
 
+APP_TITLE = "DINGUS | Chat with Logs"
+
 MODEL_PRICING = {
     "gpt-4o": {"input": 0.005, "output": 0.015},
     "gpt-4-turbo": {"input": 0.01, "output": 0.03},
@@ -12,9 +14,15 @@ MODEL_PRICING = {
 }
 LOKI_QUERY_RANGE_ENDPOINT = "/loki/api/v1/query_range"
 LOGGING_FILE = "/logs/dingus.log"
-APP_TITLE = "DINGUS | Chat with Logs"
 LOG_DATA_FILE_PATH = "/data/loki_stream.json"
+
 TRUNCATE_LOGS = int(100)
-OPENAI_MODEL = "gpt-4o-mini"
+
+QDRANT_PORT = os.getenv("QDRANT_PORT")
+QDRANT_HOST = f"{os.getenv('QDRANT_HOST')}:{QDRANT_PORT}"
+QDRANT_COLLECTION_NAME = os.getenv("QDRANT_COLLECTION_NAME")
+SENTENCE_TRANSFORMER_MODEL = "all-MiniLM-L6-v2"
+QDRANT_VECTOR_SIZE = 384  # 384D for MiniLM
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-QDRANT_HOST = "http://host.docker.internal:6333"
+OPENAI_MODEL = os.getenv("OPENAI_MODEL")
