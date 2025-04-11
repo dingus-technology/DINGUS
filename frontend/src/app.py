7aac0eedf9c9214370dad88ce31e24420165dd97
Leonwatsoni <leonhunterwatson@gmail.com>
2025-04-11 08:33:42 +0100

    🔧 chore: Fix text in app.py for clarity by removing redundant wording in the description of Dingus' functionality

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -18,7 +18,7 @@ ASSISTANT_AVATAR_URL = "/assets/logo-light.png"
 
 st.title("Chat with Dingus 💬")
 st.text(
-    "Dingus will identify issues, find causes and provide actions for problems bugs in your infrastructure."
+    "Dingus will identify issues, find causes and provide actions for bugs in your infrastructure."
 )
 
 if "messages" not in st.session_state:
